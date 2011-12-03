3f6df0f6d809efadd4d24da6a15f5b0238826727
Emily L <peppermintdove@gmail.com>
2011-12-02 23:36:53 -0800

    add a __unicode__ method for Comment

--- emily/blog/models.py
+++ emily/blog/models.py
@@ -17,3 +17,7 @@ class Comment(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField('posted at')
+    
+    def __unicode__(self):
+        return "post_id: " + self.post.id + ", title: " + self.title + ", created_at: " + str(self.created_at) + ", body: " + self.body
+        
